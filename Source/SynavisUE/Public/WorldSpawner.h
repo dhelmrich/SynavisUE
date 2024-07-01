99b3ff29d905a94439cb08e356144421d2973286
Dirk Helmrich <dnh2@hi.is>
2024-07-01 17:09:32 +0200

    Promote default material to editor property

--- Source/SynavisUE/Public/WorldSpawner.h
+++ Source/SynavisUE/Public/WorldSpawner.h
@@ -44,6 +44,9 @@ public:
   UPROPERTY(EditAnywhere, Category = "Field")
   class UBoxComponent* CropField;
 
+  UPROPERTY(EditAnywhere, Category = "Coupling")
+  UMaterial* DefaultMaterial;
+
   UPROPERTY(BlueprintReadOnly, Category = "Management")
   TArray<FObjectSpawnInstance> SpawnedObjects;
 
@@ -120,9 +123,6 @@ protected:
   AActor* HeldActor;
   UPROPERTY()
   USceneComponent* HeldComponent;
-
-  UPROPERTY()
-  UMaterial* DefaultMaterial;
 
 public:
   // Called every frame
